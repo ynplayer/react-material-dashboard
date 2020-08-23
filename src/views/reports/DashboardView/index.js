0982ebe3769fdc4c520807282a53773d482bfc16
ynplayer <38552588+ynplayer@users.noreply.github.com>
2020-08-23 11:56:21 +0700

    Update index.js

--- src/views/reports/DashboardView/index.js
+++ src/views/reports/DashboardView/index.js
@@ -32,84 +32,9 @@ const Dashboard = () => {
       title="Dashboard"
     >
       <Container maxWidth={false}>
-        <Grid
-          container
-          spacing={3}
-        >
-    <div class="gcse-search"></div>
-          <Grid
-            item
-            lg={3}
-            sm={6}
-            xl={3}
-            xs={12}
-          >
-            <Budget />
-          </Grid>
-          <Grid
-            item
-            lg={3}
-            sm={6}
-            xl={3}
-            xs={12}
-          >
-            <TotalCustomers />
-          </Grid>
-          <Grid
-            item
-            lg={3}
-            sm={6}
-            xl={3}
-            xs={12}
-          >
-            <TasksProgress />
-          </Grid>
-          <Grid
-            item
-            lg={3}
-            sm={6}
-            xl={3}
-            xs={12}
-          >
-            <TotalProfit />
-          </Grid>
-          <Grid
-            item
-            lg={8}
-            md={12}
-            xl={9}
-            xs={12}
-          >
-            <Sales />
-          </Grid>
-          <Grid
-            item
-            lg={4}
-            md={6}
-            xl={3}
-            xs={12}
-          >
-            <TrafficByDevice />
-          </Grid>
-          <Grid
-            item
-            lg={4}
-            md={6}
-            xl={3}
-            xs={12}
-          >
-            <LatestProducts />
-          </Grid>
-          <Grid
-            item
-            lg={8}
-            md={12}
-            xl={9}
-            xs={12}
-          >
-            <LatestOrders />
-          </Grid>
-        </Grid>
+    tes
+    <div class="gcse-search"></div>    
+    
       </Container>
     </Page>
   );
